7e1b7080f0726a4780a86caf1364303a73c35a68
Pablo Fraile <socdelbarca72@gmail.com>
2017-09-18 19:20:53 +0200

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script for clean the kernel source
+
+echo '################################# '
+echo '#                               # '
+echo '#  CLEANING THE KERNEL SOURCE   # '
+echo '#                               # '
+echo '################################# '
+
+make clean
+make mrproper
