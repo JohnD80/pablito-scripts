e03f85021e6c4a5d6eda27bfefe2645ca545258e
Pablo Fraile <socdelbarca72@gmail.com>
2017-10-02 19:31:51 +0200

    install-everything.sh: Init
    This scripts installs you all the necessary thing for build android in linux

---
+++ install-everything.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Script for setup the global envoirment
+# Copyright (C) 2017 Pablo Fraile Alonso (Github aka: Pablito2020)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# Run the terminal as root
+sudo su
+
+# Setup the global enviorment (necessary for build android libraries)
+sudo apt-get install git
+sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven
+sudo apt-get update
+sudo apt-get install openjdk8-jdk
+sudo apt-get install repo
